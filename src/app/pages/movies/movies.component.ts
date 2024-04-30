cc607ffdf845bb71615855e7d651821408a4b38b
Matija Milosevic <matijamilosevic1988@gmail.com>
2024-04-30 11:15:39 +0200

    Movie slider responsiveness
    Fixed responsiveness of Movie slider

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -42,25 +42,54 @@ export class MoviesComponent implements OnInit {
 
   sliderInit() {
     this.slider = new KeenSlider(this.sliderRef.nativeElement, {
-      slides: {
-        perView: 8,
-        spacing: 20,
+      slides: () => {
+        const numSlides = 20;
+        const slidesData = Array.from({ length: numSlides }, () => ({
+          size: 0.15,
+          spacing: 0.02,
+        }));
+        return slidesData;
       },
       breakpoints: {
-        '(max-width: 700px)': {
+        '(max-width: 1300px)': {
           slides: {
-            perView: 2,
+            perView: 5.5,
+            spacing: 20,
+          },
+        },
+        '(max-width: 1100px)': {
+          slides: {
+            perView: 4.5,
+            spacing: 20,
+          },
+        },
+        '(max-width: 900px)': {
+          slides: {
+            perView: 3.5,
+            spacing: 20,
+          },
+        },
+        '(max-width: 650px)': {
+          slides: {
+            perView: 2.8,
+            spacing: 20,
+          },
+        },
+        '(max-width: 500px)': {
+          slides: {
+            perView: 2.2,
             spacing: 20,
           },
         },
         '(max-width: 400px)': {
           slides: {
-            perView: 2,
+            perView: 1.8,
             spacing: 20,
           },
         },
       },
       // loop: true,
+      mode: 'snap',
     });
   }
 }
