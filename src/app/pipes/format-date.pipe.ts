337141e014c23bc1ffbfea9ab0423fa28365943f
Matija Milosevic <matijamilosevic1988@gmail.com>
2024-05-01 22:28:02 +0200

    Movie details page
    - Added style and render information for movie details
    - Added format date pipe
    - Added movie details component

---
+++ src/app/pipes/format-date.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'formatDate',
+  standalone: true,
+})
+export class FormatDatePipe implements PipeTransform {
+  transform(value: string): string {
+    const date = new Date(value);
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    return `${day}-${month}-${year}`;
+  }
+}
