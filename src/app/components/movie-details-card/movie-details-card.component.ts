26f33af87908e16d41d154417b8d7abd59325895
Matija Milosevic <matijamilosevic1988@gmail.com>
2024-05-02 15:38:27 +0200

    Movie Details Refactoring
    - Refactored movie details ( added social links, stars, tags)
    - Moved styles of movie details to separate file inside partial scss folder
    - Installed Font Awsome library

--- src/app/components/movie-details-card/movie-details-card.component.ts
+++ src/app/components/movie-details-card/movie-details-card.component.ts
@@ -1,13 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faStar,
+  faHome,
+  IconDefinition,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-movie-details-card',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, FontAwesomeModule],
   templateUrl: './movie-details-card.component.html',
   styleUrl: './movie-details-card.component.scss',
 })
 export class MovieDetailsCardComponent {
+  @HostBinding('class') class = 'movie-details-card';
   @Input() movieDetails: any;
+
+  faStar = faStar;
+  faHome = faHome;
+
+  renderStars(rating: number): IconDefinition[] {
+    return Array(Math.min(Math.floor(rating), 10)).fill(faStar);
+  }
 }
